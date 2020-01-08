9c9af21d9ac0cd434055e0370689fc8c8cb1ded2
Elliott Brannon <esbrannon@gmail.com>
2020-01-08 11:32:07 -0500

    scheduled tweet

--- sftp-retweet.py
+++ sftp-retweet.py
@@ -1,7 +1,17 @@
 import tweepy
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-api.update_status("Sending my first tweet via Tweepy!")
+sched = BlockingScheduler()
+
+@sched.scheduled_job('interval', minutes=10)
+def timed_job():
+    api.update_status("Sending my first tweet via Tweepy!")
+
+sched.start()
+
+
+

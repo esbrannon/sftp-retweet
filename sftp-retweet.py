ecc7e3317d80dda25d88e5e9177ff687e95a427e
Elliott Brannon <esbrannon@gmail.com>
2020-01-08 11:55:59 -0500

    edited sftp-retweet.py

--- sftp-retweet.py
+++ sftp-retweet.py
@@ -1,5 +1,11 @@
 import tweepy
+from os import environ
 from apscheduler.schedulers.blocking import BlockingScheduler
+
+CONSUMER_KEY = environ['CONSUMER_KEY']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_KEY = environ['ACCESS_KEY']
+ACCESS_SECRET = environ['ACCESS_SECRET']
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)

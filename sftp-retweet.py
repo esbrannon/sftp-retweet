614bf95f780a52bc7c2f8d8ed49ba1eeea6157e4
Elliott Brannon <esbrannon@gmail.com>
2020-01-08 11:18:34 -0500

    removed keys

--- sftp-retweet.py
+++ sftp-retweet.py
@@ -1,9 +1,9 @@
 import tweepy
 
-CONSUMER_KEY = 'SVPmqMF4QgOr6zDPCw2vN6Rld'
-CONSUMER_SECRET = '8mg9pmt0RWrbyKIdQS3MwymJM23fQRne3j4UmKIcshepeb7yiY'
-ACCESS_KEY = '3192182461-61si4xpOxfYoDHuABRhJT2Nkz3b0WRdwGaXvAdl'
-ACCESS_SECRET = 'IgAbEsuUczuyCqwUqRLnXy9JhNhH6tqSs5b21qgfaWmLi'
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
+ACCESS_KEY = ''
+ACCESS_SECRET = ''
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)

392d633d8d530633d8342ab0d439a0065f96511a
Elliott Brannon <esbrannon@gmail.com>
2020-01-08 13:03:01 -0500

    edited to retweet

--- sftp-retweet.py
+++ sftp-retweet.py
@@ -5,17 +5,31 @@ CONSUMER_SECRET = environ['CONSUMER_SECRET']
 ACCESS_KEY = environ['ACCESS_KEY']
 ACCESS_SECRET = environ['ACCESS_SECRET']
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-api = tweepy.API(auth)
+
+def store_last_id(tweet_id):
+    """ Store a tweet id in a file """
+    with open("lastid", "w") as fp:
+        fp.write(str(tweet_id))
+
+def get_last_id():
+    """ Read the last retweeted id from a file """
+    with open("lastid", "r") as fp:
+        return fp.read()
 
 
-#INTERVAL = 60 * 60 * 6  # tweet every 6 hours
-INTERVAL = 500  # every 15 seconds, for testing
-while True:
-    api.update_status("Sending my first tweet via Tweepy!")
-    time.sleep(INTERVAL)
+if __name__ == '__main__':
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+    api = tweepy.API(auth)
 
+    try:
+        last_id = get_last_id()
+    except FileNotFoundError:
+        print("No retweet yet")
+        last_id = None
 
-
-
+    for tweet in tweepy.Cursor(api.search, q="sftpaa", since_id=last_id).items():
+        if tweet.user.name == 'Elliott Brannon':
+            store_last_id(tweet.id)
+            tweet.retweet()
+            print(f'"{tweet.text}" was retweeted'
